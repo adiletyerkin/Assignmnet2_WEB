2501f9be186c2b1ca68d5308cf7de8b11ee022ee
Adilet Yerkin <adilet.yerkin@gmail.com>
2021-02-12 23:11:59 +0600

    all eeee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { LayoutComponent } from './layout/layout.component';
+import { AddrecordComponent } from './layout/addrecord/addrecord.component';
+import { MyresultsComponent } from './layout/myresults/myresults.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    LayoutComponent,
+    AddrecordComponent,
+    MyresultsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
